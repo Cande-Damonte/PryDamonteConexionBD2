44027a93ba0d9dbc7e7b7f6816f16ddcab583dc0
Cande Damonte <cande.damonte@gmail.com>
2025-04-11 01:59:37 -0300

    Agregar archivos de proyecto.

---
+++ ClsConexionBD.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.OleDb;
+
+
+namespace PryDamonteConexionBD2
+{
+    public class ClsConexionBD
+    {
+        OleDbCommand Comando;
+        OleDbConnection Conexion;
+        OleDbDataAdapter Adaptador;
+
+        string cadena;
+
+        public ClsConexionBD()
+        {
+            cadena = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = Database1.accdb";
+        }
+        public void ListarDatos(DataGridView DgvDatos)
+        {
+            try
+            {
+                Conexion = new OleDbConnection(cadena);
+                Comando = new OleDbCommand();
+
+                Comando.Connection = Conexion;
+                Comando.CommandType = CommandType.Text;
+                Comando.CommandText = "SELECT * FROM Contactos";
+
+                DataTable Contactos = new DataTable();
+                Adaptador = new OleDbDataAdapter(Comando);
+                Adaptador.Fill(Contactos);
+
+                DgvDatos.DataSource = Contactos;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace PryDamonteConexionBD2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DgvDatos = new System.Windows.Forms.DataGridView();
+            this.ClmNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ClmApellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ClmTelefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ClmCorreo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ClmCategoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DgvDatos
+            // 
+            this.DgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ClmNombre,
+            this.ClmApellido,
+            this.ClmTelefono,
+            this.ClmCorreo,
+            this.ClmCategoria});
+            this.DgvDatos.Location = new System.Drawing.Point(127, 67);
+            this.DgvDatos.Name = "DgvDatos";
+            this.DgvDatos.Size = new System.Drawing.Size(540, 265);
+            this.DgvDatos.TabIndex = 0;
+            // 
+            // ClmNombre
+            // 
+            this.ClmNombre.HeaderText = "Nombre";
+            this.ClmNombre.Name = "ClmNombre";
+            // 
+            // ClmApellido
+            // 
+            this.ClmApellido.HeaderText = "Apellido";
+            this.ClmApellido.Name = "ClmApellido";
+            // 
+            // ClmTelefono
+            // 
+            this.ClmTelefono.HeaderText = "Telefono";
+            this.ClmTelefono.Name = "ClmTelefono";
+            // 
+            // ClmCorreo
+            // 
+            this.ClmCorreo.HeaderText = "Correo";
+            this.ClmCorreo.Name = "ClmCorreo";
+            // 
+            // ClmCategoria
+            // 
+            this.ClmCategoria.HeaderText = "Categoria";
+            this.ClmCategoria.Name = "ClmCategoria";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DgvDatos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DgvDatos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DgvDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ClmNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ClmApellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ClmTelefono;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ClmCorreo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ClmCategoria;
+    }
+}
+
